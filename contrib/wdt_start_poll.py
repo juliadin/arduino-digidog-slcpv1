e6cc81e114ca3befcd15f34bcf693af701040882
Joel Brunenberg <joel@jjim.de>
2017-09-27 12:04:47 +0200

    Start to implement a class to abstract a watchdog device

--- contrib/wdt_start_poll.py
+++ contrib/wdt_start_poll.py
@@ -11,6 +11,70 @@ interval = 60
 target = 120
 
 
+class DigiDog(object):
+    """Abstraction of DigiDog device."""
+
+    def __init__(self, device):
+        """Contructor for Watchdog abstraction."""
+        self._device = device
+
+    def _communicate(self, write):
+        """Communicate with the device. Send 'write', return lines as array."""
+        with serial.Serial(self.device, 9600, xonxoff=False, rtscts=False, timeout=1) as sdev:
+            sdev.write(write)
+            time.sleep(0.05)
+            timeout = False
+            data = ""
+            while not timeout:
+                curr = sdev.read()
+                if not curr:
+                    timeout = True
+                else:
+                    data = data + curr
+            lines = [x for x in data.split("\n") if x != ""]
+            return lines
+
+    def command(self, command):
+        """Send command to device, return results as dict of lists."""
+        lines = self._communicate(command)
+        commands = {}
+        for line in lines:
+            try:
+                key, value = line.split(":", 1)
+            except ValueError:
+                key = "unknown"
+                value = line
+            if key not in commands:
+                commands[key] = []
+            commands[key].append(value)
+        return commands
+
+    def version(self):
+        """Return version number."""
+        results = self.command("V")
+        if "V" in results:
+            return results["V"].pop()
+        else:
+            return 0
+
+    def blocked_commands(self):
+        """Return list of blocked commands on device as list of characters."""
+        version = self.version()
+        if version >= 2:
+            results = self.command("Q")
+            if "Q" not in results:
+                return []
+            else:
+                blocked_commands = results["Q"]
+                try:
+                    blocked_commands.remove("Q")
+                except ValueError:
+                    raise ValueError("List of blocked commands is not complete.")
+                return blocked_commands
+        else:
+            raise NotImplementedError("Not impelemted in protocol version {} supported by device.".format(version))
+
+
 def calculate_bounds(reset_interval, target_timeout):
     """Calculate a range for the point in time when to reset watchdog."""
     upper_bound = target_timeout + reset_interval
@@ -62,63 +126,64 @@ def lines_from_device(device):
 
 
 detected = False
+
+with serial.Serial(WDT_DEVICE, 9600, xonxoff=False, rtscts=False, timeout=1) as wdt_dev:
+    wdt_dev.write("V")
+    for line in lines_from_device(wdt_dev):
+        if line.startswith("V:"):
+            if line.split("V:")[1].strip() >= "1":
+                print "Detected protocol version >=1.0 Watchdog on port {}".format(WDT_DEVICE)
+                detected = True
+            else:
+                print "Detected protocol version is not supported"
+        if line.startswith("U:"):
+            if line.split("U:")[1].strip() == "0":
+                print "Detected unknown device on port {}".format(WDT_DEVICE)
+            elif line.split("U:")[1].strip() == "1":
+                print "Detected DigiSpark based DigiDog on port {}".format(WDT_DEVICE)
+            else:
+                print "Detected protocol version is not known. This may or may not cause problems."
+    if not detected:
+        sys.exit(1)
+    wdt_dev.write("C")
+    for line in lines_from_device(wdt_dev):
+        if line.startswith("N:"):
+            if line.split("N:")[1].strip() == "1":
+                method = "power cycle"
+            else:
+                method = "reset"
+            print "The device is configured to recover the system by {}".format(method)
+    wdt_dev.write("S")
+    for line in lines_from_device(wdt_dev):
+        if line.startswith("F:"):
+            if line.split(":")[1].strip() == "1":
+                print "Watchdog fired since last activation"
+            else:
+                print "Watchdog did not fire since last activation"
+    wdt_dev.write("X")
+    for line in lines_from_device(wdt_dev):
+        if line.startswith("A:"):
+            if line.split(":")[1].strip() == "1":
+                print "Watchdog successfully enabled"
+            else:
+                print "Failed to enable watchdog"
+        elif line.startswith("S:"):
+            print "Timeout approximately {}s".format(int(int(line.split(":")[1])/10))
+blocked_cmds = blocked_commands(WDT_DEVICE)
+if "-" in blocked_cmds or "+" in blocked_cmds:
+    print "Device forbids adjusting the watchdog - disabled"
+    disable_adjust = True
+else:
+    disable_adjust = False
+if "x" in blocked_cmds:
+    print "Watchdog forbids stopping the timer! Can not stop timer on exit!"
+    disable_stop = True
+else:
+    disable_stop = False
+
+notice_disable_adjust = True
+
 try:
-    with serial.Serial(WDT_DEVICE, 9600, xonxoff=False, rtscts=False, timeout=1) as wdt_dev:
-        wdt_dev.write("V")
-        for line in lines_from_device(wdt_dev):
-            if line.startswith("V:"):
-                if line.split("V:")[1].strip() >= "1":
-                    print "Detected protocol version >=1.0 Watchdog on port {}".format(WDT_DEVICE)
-                    detected = True
-                else:
-                    print "Detected protocol version is not supported"
-            if line.startswith("U:"):
-                if line.split("U:")[1].strip() == "0":
-                    print "Detected unknown device on port {}".format(WDT_DEVICE)
-                elif line.split("U:")[1].strip() == "1":
-                    print "Detected DigiSpark based DigiDog on port {}".format(WDT_DEVICE)
-                else:
-                    print "Detected protocol version is not known. This may or may not cause problems."
-        if not detected:
-            sys.exit(1)
-        wdt_dev.write("C")
-        for line in lines_from_device(wdt_dev):
-            if line.startswith("N:"):
-                if line.split("N:")[1].strip() == "1":
-                    method = "power cycle"
-                else:
-                    method = "reset"
-                print "The device is configured to recover the system by {}".format(method)
-        wdt_dev.write("S")
-        for line in lines_from_device(wdt_dev):
-            if line.startswith("F:"):
-                if line.split(":")[1].strip() == "1":
-                    print "Watchdog fired since last activation"
-                else:
-                    print "Watchdog did not fire since last activation"
-        wdt_dev.write("X")
-        for line in lines_from_device(wdt_dev):
-            if line.startswith("A:"):
-                if line.split(":")[1].strip() == "1":
-                    print "Watchdog successfully enabled"
-                else:
-                    print "Failed to enable watchdog"
-            elif line.startswith("S:"):
-                print "Timeout approximately {}s".format(int(int(line.split(":")[1])/10))
-    blocked_cmds = blocked_commands(WDT_DEVICE)
-    if "-" in blocked_cmds or "+" in blocked_cmds:
-        print "Device forbids adjusting the watchdog - disabled"
-        disable_adjust = True
-    else:
-        disable_adjust = False
-    if "x" in blocked_cmds:
-        print "Watchdog forbids stopping the timer! Can not stop timer on exit!"
-        disable_stop = True
-    else:
-        disable_stop = False
-
-    notice_disable_adjust = True
-
     # Loop for resetting the watchdog
     while True:
         lower, upper = calculate_bounds(interval, target)
